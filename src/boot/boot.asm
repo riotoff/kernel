d4e333bab610a107229ee6c49610ae402ea81329
Arseniy Rayotov <daxkod3243@gmail.com>
2025-02-04 23:20:36 +0300

    - kernel/src/boot/boot.asm: Updated reading kernel file (still doesn't work)

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -16,7 +16,7 @@ nop
 
 msg db 'boot load sy0.', 13, 10, 0
 filename db 'kernel.bin', 0
-buffer equ 0x1000
+buffer equ 0x9000
 
 start:
     xor ax, ax
@@ -28,35 +28,51 @@ start:
     mov si, msg
     call print_string
 
-    mov ax, 0xFFFF
-    mov ds, ax
-    mov [0x80], byte 128
+    mov ax, 0x0201
+    mov cx, 2
+    mov dx, 0
+    mov bx, buffer
+    int 0x13
+    jc error_reading_sector
 
-    mov dx, filename
-    mov cx, 0
-    mov ah, 4Eh
-    int 21h
-    jc error_file_not_found
-
-    mov bx, 0x81
-    mov di, bx
+    mov si, filename
+    mov di, buffer + 32
+    mov cx, 14
+.next_entry:
+    push cx
     mov cx, 11
-    rep movsb
+    repe cmpsb
+    je found_file
+    add di, 32
+    pop cx
+    loop .next_entry
+    jmp file_not_found
 
-    mov bx, 0xFFFF
-    add bx, 1Ah
-    mov ax, [bx]
+found_file:
+    mov ax, [di + 26]
+    mov word [cluster], ax
+    mov ax, [di + 20]
     mov word [file_size], ax
 
-    mov dx, buffer
-    mov bx, 0xFFFF
-    mov ah, 3Fh
-    int 21h
+.read_cluster:
+    mov ax, 0x201
+    mov bx, buffer
+    call read_fat
     jc error_reading_file
+    call read_data
+    jc error_reading_file
+    cmp word [cluster], 0xFFF8
+    ja .read_cluster
 
     jmp buffer
 
-error_file_not_found:
+error_reading_sector:
+    mov si, reading_error_msg
+    call print_string
+    cli
+    hlt
+
+file_not_found:
     mov si, file_not_found_msg
     call print_string
     cli
@@ -86,6 +102,60 @@ print_string:
 .done:
     ret
 
+read_fat:
+    push ax
+    push bx
+    push cx
+    push dx
+    push es
+
+    mov ax, 0x1000
+    mov es, ax
+    mov bx, [cluster]
+    shl bx, 1
+    add bx, 0x50
+    mov ax, [es:bx]
+    mov [cluster], ax
+
+    pop es
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    clc
+    ret
+
+read_data:
+    push ax
+    push bx
+    push cx
+    push dx
+    push es
+
+    mov ax, 0x1000
+    mov es, ax
+    mov bx, [cluster]
+    sub bx, 2
+    shl bx, 9
+    add bx, buffer
+    mov ax, 0x0201
+    mov cx, 1
+    mov dx, 0
+    int 0x13
+    jc .error
+
+    pop es
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    clc
+    ret
+.error:
+    stc
+    ret
+
+cluster dw 0
 file_size dw 0
 
 times 510 - ($ -$$) db 0
